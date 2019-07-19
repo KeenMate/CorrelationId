b24716ed6c53e1ac4e6658abe1502a485df20495
TheMaikXX <jakabfilip.fj@gmail.com>
2019-07-20 00:12:37 +0200

    Changed CorrelationId from string to Guid and store it in Items of HttpContext as well

--- src/CorrelationId/CorrelationIdExtensions.cs
+++ src/CorrelationId/CorrelationIdExtensions.cs
@@ -1,72 +1,88 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Options;
 using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CorrelationId
 {
-    /// <summary>
-    /// Extension methods for the CorrelationIdMiddleware.
-    /// </summary>
-    public static class CorrelationIdExtensions
-    {
-        /// <summary>
-        /// Enables correlation IDs for the request.
-        /// </summary>
-        /// <param name="app"></param>
-        /// <returns></returns>
-        public static IApplicationBuilder UseCorrelationId(this IApplicationBuilder app)
-        {
-            if (app == null)
-            {
-                throw new ArgumentNullException(nameof(app));
-            }
+	/// <summary>
+	/// Extension methods for the CorrelationIdMiddleware.
+	/// </summary>
+	public static class CorrelationIdExtensions
+	{
+		/// <summary>
+		/// Enables correlation IDs for the request.
+		/// </summary>
+		/// <param name="app"></param>
+		/// <returns></returns>
+		public static IApplicationBuilder UseCorrelationId(this IApplicationBuilder app)
+		{
+			if (app == null)
+			{
+				throw new ArgumentNullException(nameof(app));
+			}
 
-            return app.UseCorrelationId(new CorrelationIdOptions());
-        }
+			return app.UseCorrelationId(new CorrelationIdOptions());
+		}
 
-        /// <summary>
-        /// Enables correlation IDs for the request.
-        /// </summary>
-        /// <param name="app"></param>
-        /// <param name="header">The header field name to use for the correlation ID.</param>
-        /// <returns></returns>
-        public static IApplicationBuilder UseCorrelationId(this IApplicationBuilder app, string header)
-        {
-            if (app == null)
-            {
-                throw new ArgumentNullException(nameof(app));
-            }
+		/// <summary>
+		/// Enables correlation IDs for the request.
+		/// </summary>
+		/// <param name="app"></param>
+		/// <param name="header">The header field name to use for the correlation ID.</param>
+		/// <returns></returns>
+		public static IApplicationBuilder UseCorrelationId(this IApplicationBuilder app, string header)
+		{
+			if (app == null)
+			{
+				throw new ArgumentNullException(nameof(app));
+			}
 
-            return app.UseCorrelationId(new CorrelationIdOptions
-            {
-                Header = header
-            });
-        }
+			return app.UseCorrelationId(new CorrelationIdOptions
+			{
+				Header = header
+			});
+		}
 
-        /// <summary>
-        /// Enables correlation IDs for the request.
-        /// </summary>
-        /// <param name="app"></param>
-        /// <param name="options"></param>
-        /// <returns></returns>
-        public static IApplicationBuilder UseCorrelationId(this IApplicationBuilder app, CorrelationIdOptions options)
-        {
-            if (app == null)
-            {
-                throw new ArgumentNullException(nameof(app));
-            }
+		/// <summary>
+		/// Enables correlation IDs for the request.
+		/// </summary>
+		/// <param name="app"></param>
+		/// <param name="options"></param>
+		/// <returns></returns>
+		public static IApplicationBuilder UseCorrelationId(this IApplicationBuilder app, CorrelationIdOptions options)
+		{
+			if (app == null)
+			{
+				throw new ArgumentNullException(nameof(app));
+			}
 
-            if (options == null)
-            {
-                throw new ArgumentNullException(nameof(options));
-            }
+			if (options == null)
+			{
+				throw new ArgumentNullException(nameof(options));
+			}
 
-            if (app.ApplicationServices.GetService(typeof(ICorrelationContextFactory)) == null)
-            {
-                throw new InvalidOperationException("Unable to find the required services. You must call the AddCorrelationId method in ConfigureServices in the application startup code.");
-            }
+			if (app.ApplicationServices.GetService(typeof(ICorrelationContextFactory)) == null)
+			{
+				throw new InvalidOperationException(
+					"Unable to find the required services. You must call the AddCorrelationId method in ConfigureServices in the application startup code.");
+			}
 
-            return app.UseMiddleware<CorrelationIdMiddleware>(Options.Create(options));
-        }
-    }
+			return app.UseMiddleware<CorrelationIdMiddleware>(Options.Create(options));
+		}
+
+		/// <summary>
+		/// Returns CorrelationId from HttpContext.Items collection
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="itemName">Name of the Item containing CorrelationId</param>
+		/// <returns>CorrelationId or empty string</returns>
+		public static Guid GetCorrelationId(this HttpContext context, string itemName)
+		{
+			return context.Items.ContainsKey(itemName)
+				? (Guid) context.Items[itemName]
+				: Guid.Empty;
+		}
+	}
 }
--- src/CorrelationId/CorrelationIdMiddleware.cs
+++ src/CorrelationId/CorrelationIdMiddleware.cs
@@ -1,79 +1,89 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Primitives;
 
 namespace CorrelationId
 {
-    /// <summary>
-    /// Middleware which attempts to reads / creates a Correlation ID that can then be used in logs and 
-    /// passed to upstream requests.
-    /// </summary>
-    public class CorrelationIdMiddleware
-    {
-        private readonly RequestDelegate _next;
-        private readonly CorrelationIdOptions _options;
+	/// <summary>
+	/// Middleware which attempts to reads / creates a Correlation ID that can then be used in logs and 
+	/// passed to upstream requests.
+	/// </summary>
+	public class CorrelationIdMiddleware
+	{
+		private readonly RequestDelegate _next;
+		private readonly CorrelationIdOptions _options;
 
-        /// <summary>
-        /// Creates a new instance of the CorrelationIdMiddleware.
-        /// </summary>
-        /// <param name="next">The next middleware in the pipeline.</param>
-        /// <param name="options">The configuration options.</param>
-        public CorrelationIdMiddleware(RequestDelegate next, IOptions<CorrelationIdOptions> options)
-        {
-            _next = next ?? throw new ArgumentNullException(nameof(next));
-            _options = options.Value ?? throw new ArgumentNullException(nameof(options));
-        }
+		/// <summary>
+		/// Creates a new instance of the CorrelationIdMiddleware.
+		/// </summary>
+		/// <param name="next">The next middleware in the pipeline.</param>
+		/// <param name="options">The configuration options.</param>
+		public CorrelationIdMiddleware(RequestDelegate next, IOptions<CorrelationIdOptions> options)
+		{
+			_next = next ?? throw new ArgumentNullException(nameof(next));
+			_options = options.Value ?? throw new ArgumentNullException(nameof(options));
+		}
 
-        /// <summary>
-        /// Processes a request to synchronise TraceIdentifier and Correlation ID headers. Also creates a 
-        /// <see cref="CorrelationContext"/> for the current request and disposes of it when the request is completing.
-        /// </summary>
-        /// <param name="context">The <see cref="HttpContext"/> for the current request.</param>
-        /// <param name="correlationContextFactory">The <see cref="ICorrelationContextFactory"/> which can create a <see cref="CorrelationContext"/>.</param>
-        public async Task Invoke(HttpContext context, ICorrelationContextFactory correlationContextFactory)
-        {
-            var correlationId = SetCorrelationId(context);
+		/// <summary>
+		/// Processes a request to synchronise TraceIdentifier and Correlation ID headers. Also creates a 
+		/// <see cref="CorrelationContext"/> for the current request and disposes of it when the request is completing.
+		/// </summary>
+		/// <param name="context">The <see cref="HttpContext"/> for the current request.</param>
+		/// <param name="correlationContextFactory">The <see cref="ICorrelationContextFactory"/> which can create a <see cref="CorrelationContext"/>.</param>
+		public async Task Invoke(HttpContext context, ICorrelationContextFactory correlationContextFactory)
+		{
+			var correlationId = SetCorrelationId(context);
 
-            if (_options.UpdateTraceIdentifier)
-                context.TraceIdentifier = correlationId;
+			if (_options.UpdateTraceIdentifier)
+				context.TraceIdentifier = correlationId.ToString();
+			
+			if (!context.Items.ContainsKey(_options.HttpContextItemName))
+				context.Items.Add(_options.HttpContextItemName, correlationId);
 
-            correlationContextFactory.Create(correlationId, _options.Header);
+			correlationContextFactory.Create(correlationId.ToString(), _options.Header);
 
-            if (_options.IncludeInResponse)
-            {
-                // apply the correlation ID to the response header for client side tracking
-                context.Response.OnStarting(() =>
-                {
-                    if (!context.Response.Headers.ContainsKey(_options.Header))
-                    {
-                        context.Response.Headers.Add(_options.Header, correlationId);
-                    }
+			if (_options.IncludeInResponse)
+			{
+				// apply the correlation ID to the response header for client side tracking
+				context.Response.OnStarting(() =>
+				{
+					if (!context.Response.Headers.ContainsKey(_options.Header))
+					{
+						context.Response.Headers.Add(_options.Header, correlationId.ToString());
+					}
 
-                    return Task.CompletedTask;
-                });
-            }
+					return Task.CompletedTask;
+				});
+			}
 
-            await _next(context);
+			await _next(context);
 
-            correlationContextFactory.Dispose();
-        }
+			correlationContextFactory.Dispose();
+		}
 
-        private StringValues SetCorrelationId(HttpContext context)
-        {
-            var correlationIdFoundInRequestHeader = context.Request.Headers.TryGetValue(_options.Header, out var correlationId);
+		private Guid SetCorrelationId(HttpContext context)
+		{
+			if (!context.Request.Headers.TryGetValue(_options.Header, out var correlationId)
+					|| !Guid.TryParse(correlationId.FirstOrDefault(), out var cid))
+				cid = GenerateCorrelationId();
+			
+			// if (RequiresGenerationOfCorrelationId(correlationIdFoundInRequestHeader, correlationId))
+			// 	correlationId = GenerateCorrelationId();
 
-            if (RequiresGenerationOfCorrelationId(correlationIdFoundInRequestHeader, correlationId))
-                correlationId = GenerateCorrelationId(context.TraceIdentifier);
+			return cid;
+		}
 
-            return correlationId;
-        }
+		private static bool RequiresGenerationOfCorrelationId(bool idInHeader, StringValues idFromHeader) =>
+			!idInHeader || StringValues.IsNullOrEmpty(idFromHeader);
 
-        private static bool RequiresGenerationOfCorrelationId(bool idInHeader, StringValues idFromHeader) => 
-            !idInHeader || StringValues.IsNullOrEmpty(idFromHeader);
+		private Guid GenerateCorrelationId() => Guid.NewGuid();
 
-        private StringValues GenerateCorrelationId(string traceIdentifier) => 
-            _options.UseGuidForCorrelationId || string.IsNullOrEmpty(traceIdentifier) ? Guid.NewGuid().ToString() : traceIdentifier;
-    }
+		// private StringValues GenerateCorrelationId(string traceIdentifier) =>
+		// 	_options.UseGuidForCorrelationId || string.IsNullOrEmpty(traceIdentifier)
+		// 		? Guid.NewGuid().ToString()
+		// 		: traceIdentifier;
+	}
 }
